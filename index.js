576e053ee4d359fc1aa5c824f480956cc02060a9
Kati Schwartz <114843679+LawdyMissKati@users.noreply.github.com>
2023-10-31 12:41:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,95 +1,112 @@
-$(".form-check-input").click(function() {
-    $(".submit").css("background-color", "green");
+// ************************************************************************************ Screening page ************************************************************************************
+// $(function() {
+//   for (var i = 1; i <=8; i + 2) {
+//     $(".sample" + i).click(function() {
+//       $("#teal" + i).css("opacity", "0.5");
+//     });
+//   };
+// });
+
+$(function() {
+  $("#screening_tile").css("background-color", "#007AB6");
 });
 
+$(function() {
+  $(".sample1").click(function() {
+    $("#teal1").css("opacity", "0.5");
+  });
+});
+
+$(function() {
+  $(".sample2").click(function() {
+    $("#pink2").css("opacity", "0.5");
+  });
+});
+
+
+// ************************************************************************************ Reading page ************************************************************************************
+
+//turn submit button green when resulting option is selected
+$(".form-check-input").click(function() {
+  $(".submit").css("background-color", "green");
+});
+
+
 // turn reading tile blue when on pre-reading or reading page
-$(document).ready(function() {
-  $("#reading-tile").css("background-color", "#007AB6");
+$(function() {
+  $("#reading_tile").css("background-color", "#007AB6");
 });
 
 // toggle lighting
 var select_illumination_dropdown = document.getElementById("selectIllumination");
 select_illumination_dropdown.addEventListener("change", function() {
   var selectedIllumination = select_illumination_dropdown.value;
-$("#lighting").attr("src", selectedIllumination);
+  $("#lighting").attr("src", selectedIllumination);
 });
-
-
 
 // New Pickpoint button
-$(document).ready(function() {
-    $("#targetElement").on("contextmenu", function(e) {
-      // hides button until right click in plate image
-      e.preventDefault();
-      // shows button at location of right click
-      $(".context-menu").css({
-        top: e.pageY + "px",
-        left: e.pageX + "px"
-      }).show();
-    });
+$(function() {
+  $(".pic").on("contextmenu", function(event) {
+    // hides button until right click in plate image
+    event.preventDefault();
+    // shows button at location of right click
+    $(".context-menu").css({
+      top: event.pageY + "px",
+      left: event.pageX + "px"
+    }).show();
+  });
 
-    // Opens New Pickpoint modal when button is clicked 
-    $("#pickpointBtn").on("click", function() {
-      $("#modal-pickpoint").modal("show");
-    });
-
-    $('#modal-pickpoint').on('hidden.bs.modal', function () {
-      // When the modal is closed, hide the button
-      $('#pickpointBtn').hide();
-    });
-
-    // Add button click event
-    $("#btnAdd").on("click", function() {
-      $("#pickpointBtn").hide();
-      $("#circle").show().draggable();
-      $("#modal-pickpoint").modal("hide");
-    });
-
-    // Cancel button click event
-    $("#btnCancel").on("click", function() {
-      $("#pickpointBtn").hide();
-      $("#pickpoint-modal").modal("hide");
-    });
+  // Opens New Pickpoint modal when button is clicked 
+  $("#pickpointBtn").on("click", function() {
+    $("#modal-pickpoint").modal("show");
   });
 
 
 
-// store options selected in new pickpoint modal
-var select_isolate_dropdown = document.getElementById("isolate");
-var select_quantity_dropdown = document.getElementById("quantity");
-var selectedIsolate, selectedQuantity;
-var pickpointpointSummary = document.getElementById("pickpointSummary");
+$(function() {
+  var selectedIsolate, selectedQuantity, selectedWorkup, pickpointSummary;
 
-select_isolate_dropdown.addEventListener("change", updatePickpointSummary);
-select_quantity_dropdown.addEventListener("change", updatePickpointSummary);
-
-$(document).ready(function() {
   $("#btnAdd").click(function() {
-    var selectedWorkup = $("input[name='test_button']:checked").val();
-    console.log(selectedWorkup);
+    selectedIsolate = $("#isolate").find(":selected").text();
+    selectedQuantity = $("#quantity").find(":selected").text();
+    selectedWorkup = $("input[name='workup_options']:checked").val();
+    pickpointpointSummary = document.getElementById("pickpointSummary");
+    pickpointpointSummary.innerHTML = selectedIsolate + "<br>" + "1) " + selectedWorkup + "<br>" + "Isolate: A" + "<br>" + "Load: " + selectedQuantity;
+  }); 
+});
+
+  // show pickpoint data on hover over circle
+  $(function() {
+    $("#circle").hover(
+      function() {
+        // mouse enter event: show pickpoint summary
+        $(this).find("#pickpointSummary").show();
+      },
+      function() {
+        // mouse leave event: hide the pickpoint summary
+        $(this).find("#pickpointSummary").hide();
+      }
+    );
+  });
+
+  $('#modal-pickpoint').on('hidden.bs.modal', function () {
+    // When the modal is closed, hide the button
+    $('#pickpointBtn').hide();
+  });
+
+  // Add button click event
+  $("#btnAdd").on("click", function() {
+    $("#pickpointBtn").hide();
+    $("#circle").show();
+    $("#modal-pickpoint").modal("hide");
+  });
+
+  // Cancel button click event
+  $("#btnCancel").on("click", function() {
+    $("#pickpointBtn").hide();
+    $("#pickpoint-modal").modal("hide");
   });
 });
 
-// displays pickpoint summary
-function updatePickpointSummary() {
-  selectedIsolate = select_isolate_dropdown.value;
-  selectedQuantity = select_quantity_dropdown.value;
-
-  pickpointpointSummary.innerHTML = selectedIsolate + "<br>" + "1) Selected Test(s)<br>Isolate: A<br>Load: " + selectedQuantity;
-};
-
-// show pickpoint summary on hover over circle
-$(document).ready(function() {
-  $("#circle").hover(
-    function() {
-      // mouse enter event: show pickpoint summary
-      $(this).find("#pickpointSummary").show();
-    },
-    function() {
-      // mouse leave event: hide the pickpoint summary
-      $(this).find("#pickpointSummary").hide();
-    }
-  );
-});
 
 
